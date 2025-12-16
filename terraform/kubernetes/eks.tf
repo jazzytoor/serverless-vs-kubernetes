ef551eab80e0674bedc6ec14d761771a29ef79ca
jazz toor <jazztoor@jazz.local>
2025-12-16 14:02:12 +0000

    feat: k8s implementation

---
+++ terraform/kubernetes/eks.tf
@@ -0,0 +1,20 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = var.service
+  kubernetes_version = "1.33"
+
+  endpoint_public_access                   = true
+  enable_cluster_creator_admin_permissions = true
+
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  tags = local.default_tags
+}
