52f0d6d7feb38b08c571d4144a73c0feff694639
jazz toor <jazztoor@jazz.local>
2025-12-22 16:35:12 +0000

    feat: create eks stack

--- terraform/kubernetes/vpc.tf
+++ terraform/kubernetes/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = var.service
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 
